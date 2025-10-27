11bfa61a6a749f6c1d8f5a89d4ce7f13a4793f34
zeeshan1853 <zeeshan1853@gmail.com>
2025-10-27 17:24:19 +0500

    secret test

--- config.js
+++ config.js
@@ -5,3 +5,5 @@ const normalVar = 'just_a_normal_string_123';
 # test_config.py
 const API_KEY = "sk-proj-test123456789abcdefghijklmnopqrstuvwxyz";
 const database_url = "postgresql://user:pass@localhost/db";
+
+const AWS_ACCESS_SECRET_KEY = 'UpUbsQANRHLf2uuQ7QOlNXPbbtV5fmseW/GgT5F/'
